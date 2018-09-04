c1f25586f948d72c85612076123fb14c196f6c56
Rodri Valdivia <rodri@MacBook-Pro-de-Rodri.local>
2018-09-04 18:04:24 -0300

    update de la app con lista de pregunta sin andar

--- app/screens/ProposalScreen.js
+++ app/screens/ProposalScreen.js
@@ -6,26 +6,33 @@ import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elemen
 // import { Proveedores } from '../models/Proveedores';
 
 class ProposalScreen extends React.Component {
-
   constructor(props){
     super(props);
     this.state = {
-      preguntas: ['pregunta 1', 'pregunta 2'],
+      preguntas: [{ s: 'pregunta 1'}, { s: 'pregunta 2'}, { s: 'pregunta 3'}],
     }
 
   }
 
   
   render() {
+
     return (
       <View>
         <Header centerComponent={{ text: 'Nueva Propuesta', style: { fontSize: 18, color: '#fff' } }} />
-        <ScrollView >
-          <FormLabel>Pregunta #1</FormLabel>
-          <FormInput onChangeText={console.log("change")}/>
-          <FormValidationMessage>Error message</FormValidationMessage>
-          <FormLabel>Pregunta #2</FormLabel>
-          <FormInput onChangeText={console.log("change")}/>
+        <ScrollView>
+          {/*{
+            this.state.preguntas.map((pregunta) => (
+              <div>
+                <FormLabel>{ {pregunta.s} }</FormLabel>
+                <FormInput onChangeText={console.log("change")}/>
+                <FormValidationMessage>Error message</FormValidationMessage>
+              </div>
+            ))
+          }*/}
+          <FormLabel>Pregunta random</FormLabel>
+                <FormInput onChangeText={console.log("change")}/>
+                <FormValidationMessage>Error message</FormValidationMessage>
         </ScrollView>
       </View>
     );
